d3b2d91d1ec06d52bbf9a13a1a8ab514ae96027f
Pavel Koryakin <unclekoin@gmail.com>
2021-05-26 16:36:04 +0300

    feat: add ExpensesFilter component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css"
 
 const ExpenseItem = ( props ) => {
-  const { amount, date } = props;
-  const [title, setTitle] = useState( props.title )
-
-  const clickHandler = () => {
-    setTitle('new ' + title )
-  }
+  const { title, amount, date } = props;
 
   return (
     <Card className="expense-item">
@@ -18,7 +13,6 @@ const ExpenseItem = ( props ) => {
         <h2>{ title }</h2>
         <div className="expense-item__price">${ amount }</div>
       </div>
-      <button onClick={ clickHandler }>Change Title</button>
     </Card>
   )
 }
